ed9962e9c23881a023eb10dcd262518ce9659eb7
Unknow Hacker <unhack9582@gmail.com>
2020-10-24 11:44:26 +0000

    Укоротила

--- main.py
+++ main.py
@@ -4,35 +4,29 @@ a = ['R', 'P', 'S']
 print("Hi! Do you want to play with a person or a computer?(computer = 1, person = 2)")
 s = int(input())
 v = {'R':'S', 'S':'P', 'P':'R'}
+f = False
+while(f == False):
+  print("Enter the first player's decision:")
+  c = input()
+  if(c in a): f = True
 if(s == 2):
-  f = False
-  while(f == False):
-    print("Enter the first player's decision:")
-    c = input()
-    if(c in a): f = True
   f = False
   while(f == False):
     print("Enter the second player's decision:")
     d = input()
-    if(d in a): f = True
-  if(v[c] == d):
-    print("1st player wins (⌐■_■)")
-  elif(v[d] == c):
-    print("2nd player wins (⌐■_■)")
-  else:
-    print("Draw")
+    if(d in a): f = True  
 else:
-  f = False
-  while(f == False):
-    print("Enter the player's decision:")
-    c = input()
-    if(c in a): f = True
   x = random.randint(0, 2)
   d = a[x]
   print("Computer's decision -", d)
-  if(v[c] == d):
-    print("player wins (￣^￣)ゞ")
-  elif(v[d] == c):
-    print("computer wins (╯°益°)╯彡┻━┻")
+if(v[c] == d):
+  if(s == 2):
+    print("1st player wins (⌐■_■)")
+  else: print("player wins (￣^￣)ゞ")
+elif(v[d] == c):
+  if(s == 2):
+    print("2nd player wins (⌐■_■)")
   else:
-    print("Draw")
+    print("computer wins (╯°益°)╯彡┻━┻")
+else:
+  print("Draw")
