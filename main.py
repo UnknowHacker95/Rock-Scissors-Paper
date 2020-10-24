58074925bdbb4317c5d869406927b73c5a371407
Unknow Hacker <unhack9582@gmail.com>
2020-10-24 10:53:35 +0000

    убрала двойной вывод

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ f = False
 while(f == False):
   print("Enter the first player's decision:")
   c = input()
-  if(c not in a): print("Enter the first player's decision:")
-  else: f = True
+  if(c in a): f = True
 f = False
 while(f == False):
   print("Enter the second player's decision:")
   d = input()
-  if(d not in a): print("Enter the second player's decision:")
-  else: f = True
+  if(d in a): f = True
 if(c == 'P' and d == 'R') or (c == 'Paper' and d == 'Rock'):
   print("1st player wins")
 elif(d == 'P' and c == 'R') or (d == 'Paper' and c == 'Rock'):
