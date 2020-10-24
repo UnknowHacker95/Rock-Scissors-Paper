5dd5c2ab09f8b6385c3546015c4ea8a73e1139f6
Unknow Hacker <unhack9582@gmail.com>
2020-10-24 11:03:03 +0000

    добавлена игра с компьютером

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
-a = ['R', 'P', 'S', 'Rock', 'Paper', 'Scissors']
-print("Hi!")
-f = False
-while(f == False):
-  print("Enter the first player's decision:")
-  c = input()
-  if(c in a): f = True
-f = False
-while(f == False):
-  print("Enter the second player's decision:")
-  d = input()
-  if(d in a): f = True
-if(c == 'P' and d == 'R') or (c == 'Paper' and d == 'Rock'):
-  print("1st player wins")
-elif(d == 'P' and c == 'R') or (d == 'Paper' and c == 'Rock'):
-  print("2nd player wins")
-elif(c == 'R' and d == 'S') or (c == 'Rock' and d == 'Scissors'):
-  print("1st player wins")
-elif(c == 'S' and d == 'R') or (c == 'Scissors' and d == 'Rock'):
-  print("2nd player wins")
-elif(c == 'S' and d == 'P') or (c == 'Scissors' and d == 'Paper'):
-  print("1st player wins")
-elif(c == 'P' and d == 'S') or (c == 'Paper' and d == 'Scissors'):
-  print("2nd player wins")
-else: print("Draw")
+import random
+
+a = ['R', 'P', 'S']
+print("Hi! Do you want to play with a person or a computer?(computer = 1, person = 2)")
+s = int(input())
+if(s == 2):
+  f = False
+  while(f == False):
+    print("Enter the first player's decision:")
+    c = input()
+    if(c in a): f = True
+  f = False
+  while(f == False):
+    print("Enter the second player's decision:")
+    d = input()
+    if(d in a): f = True
+  if(c == 'P' and d == 'R'):
+    print("1st player wins")
+  elif(d == 'P' and c == 'R'):
+    print("2nd player wins")
+  elif(c == 'R' and d == 'S'):
+    print("1st player wins")
+  elif(c == 'S' and d == 'R'):
+    print("2nd player wins")
+  elif(c == 'S' and d == 'P'):
+    print("1st player wins")
+  elif(c == 'P' and d == 'S'):
+    print("2nd player wins")
+  else:
+    print("Draw")
+else:
+  f = False
+  while(f == False):
+    print("Enter the player's decision:")
+    c = input()
+    if(c in a): f = True
+  x = random.randint(0, 2)
+  d = a[x]
+  if(c == 'P' and d == 'R'):
+    print("player wins")
+  elif(d == 'P' and c == 'R'):
+    print("computer wins")
+  elif(c == 'R' and d == 'S'):
+    print("player wins")
+  elif(c == 'S' and d == 'R'):
+    print("computer wins")
+  elif(c == 'S' and d == 'P'):
+    print("player wins")
+  elif(c == 'P' and d == 'S'):
+    print("computer wins")
+  else:
+    print("Draw")
